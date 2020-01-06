41c7f7253333472d423dc7bf0d8f6b343595c081
lizhipeng <lizhipeng@pansoft.com>
2020-01-06 20:30:42 +0800

    first commit

---
+++ src/UseCallback.js
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react';
+const functionsCounter = new Set()
+// https://flaviocopes.com/react-hook-usecallback/
+const Counter = () => {
+  const [count, setCount] = useState(0)
+  const [otherCounter, setOtherCounter] = useState(0)
+  
+  const increment = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+  const decrement = useCallback(() => {
+    setCount(count - 1)
+  }, [count])
+  const incrementOtherCounter = useCallback(() => {
+    setOtherCounter(otherCounter + 1)
+  }, [otherCounter])
+
+  /*
+  const increment = (() => {
+    setCount(count + 1)
+  })
+  const decrement = (() => {
+    setCount(count - 1)
+  })
+  const incrementOtherCounter = (() => {
+    setOtherCounter(otherCounter + 1)
+  })
+  */
+
+  functionsCounter.add(increment)
+  functionsCounter.add(decrement)
+  functionsCounter.add(incrementOtherCounter)
+
+  alert(functionsCounter.size)
+  
+  return (
+    <>
+      Count: {count}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={incrementOtherCounter}>incrementOtherCounter</button>
+    </>
+  )
+}
+
+export default Counter;
