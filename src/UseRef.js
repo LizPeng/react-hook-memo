4af2f1e851b40bc67c76582c6b0e6c16744bec5d
lizhipeng <lizhipeng@pansoft.com>
2020-01-10 16:35:44 +0800

    add callback useRef

--- src/UseRef.js
+++ src/UseRef.js
@@ -17,10 +17,28 @@ function TextInputWithFocusButton() {
   console.log(inputEL); // null
   return (
     <>
-      <input ref={inputEL} type="text"/>
+      <input ref={inputEL} type="text"/> 
       <button onClick={onButtonClick}>Focus the input</button>
     </>
   )
 }
 
+// callback ref
+// In this example, the callback ref will be called only when the component mounts and unmounts
+function MeasureExample() {
+  const [height, setHeight] = useState(0);
+
+  const measuredRef = useCallback(node => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1 ref={measuredRef}>Hello, world</h1>
+      <h2>The above header is {Math.round(height)}px tall</h2>
+    </>
+  );
+}
 export default TextInputWithFocusButton;
